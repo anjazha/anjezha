d8a52b657dfa743a72f337527019d74f212031d4
MohmedAref31 <mohmedwork2003@gmail.com>
2024-10-06 17:28:21 +0300

    update task applications features and add some data validations

--- src/Application/interfaces/TaskApplication/ITaskApplicationRepository.ts
+++ src/Application/interfaces/TaskApplication/ITaskApplicationRepository.ts
@@ -5,4 +5,6 @@ export interface ITaskApplicationRepository {
     getApplications(taskId : number | null , taskerId : number | null) : Promise<TaskApplication[]>;
     // getApplicationsByTaskerId(taskerId : number) : Promise<TaskApplication[]>;
     acceptApplication(appId : number) : Promise<boolean>;
+    checkIfTaskerApplied(taskId : number, taskerId : number) : Promise<boolean>;
+    checkifTaskerIsTaskOwner(taskId : number, taskerId : number) : Promise<boolean>;
 }
