8ef582ee96e4517b00b6be37fe507ea315acc174
MohmedAref31 <mohmedwork2003@gmail.com>
2024-10-21 11:47:29 +0300

    add seeders and dummy data

--- src/Infrastructure/database/migrations/index.ts
+++ src/Infrastructure/database/migrations/index.ts
@@ -4,7 +4,7 @@ import { migrate } from "postgres-migrations";
 async function runMigrations(): Promise<void> {
   await connectDB();
   try {
-    await migrate({ client: pgClient }, "src/Infrastructure/migrations");
+    await migrate({ client: pgClient }, "src/Infrastructure/database/migrations");
     console.log("migration complete");
   }catch (error) {
     console.log(error)
